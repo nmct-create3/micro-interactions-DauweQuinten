40cfb5e9a6dd351db167ee2c9bdf0c0622c2c83f
DauweQuinten <quinten.dauwe@student.howest.be>
2022-10-06 14:59:30 +0200

    feat: password toggle

--- script/app.js
+++ script/app.js
@@ -1,13 +1,22 @@
-function handleFloatingLabel() {
-   
-}
+function handleFloatingLabel() {}
 
 function handlePasswordSwitcher() {
-    
+  const togglePasswordInput = document.querySelector(
+    '.c-toggle-password__checkbox'
+  );
+  togglePasswordInput.addEventListener('change', function () {
+    console.log('👀');
+    const passwordInput = document.querySelector('.js-password');
+    if (this.checked) {
+      passwordInput.type = 'text';
+    } else {
+      passwordInput.type = 'password';
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded!');
-    handleFloatingLabel();
-    handlePasswordSwitcher();
+  console.log('Script loaded!');
+  handleFloatingLabel();
+  handlePasswordSwitcher();
 });
